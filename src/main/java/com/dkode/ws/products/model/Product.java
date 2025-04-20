1e86865f317fe70ae4405710eef06922291a24ae
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-04-20 17:39:52 +0530

    Product created event class and lombok

--- src/main/java/com/dkode/ws/products/model/Product.java
+++ src/main/java/com/dkode/ws/products/model/Product.java
@@ -1,4 +1,16 @@
 package com.dkode.ws.products.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class Product {
+
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
 }
